9e556331d6ab2f147fe1d7c94f7fc8a4c1a0e5b4
1985devops <sticp9@yandex.ru>
2026-09-21 12:11:17 +0200

    Настроить удаленный бэкенд S3 с параметром use_lockfile

--- 04/demonstration1/vms/providers.tf
+++ 04/demonstration1/vms/providers.tf
@@ -5,6 +5,24 @@ terraform {
     }
   }
   required_version = ">=1.12.0"
+
+  backend "s3" {
+    bucket = "tfstate-1985devops"
+    key    = "terraform.tfstate"
+    region = "ru-central1"
+
+    # Встроенный механизм блокировок (Terraform >= 1.6)
+    use_lockfile = true
+
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
 
 provider "yandex" {
@@ -13,4 +31,3 @@ provider "yandex" {
   folder_id = var.folder_id
   zone      = var.zone
 }
-
